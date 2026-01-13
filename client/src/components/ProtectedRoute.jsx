d0acde26a33e1071605a6bb8b3713b057485ecaf
nhanmmo2005 <hopdangnhan@dtu.edu.vn>
2026-01-14 00:09:16 +0700

    Nhân Update scr client

--- client/src/components/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute.jsx
@@ -1,9 +1,14 @@
-import React from "react";
 import { Navigate } from "react-router-dom";
+import { getToken, getUser } from "../auth";
 
-export default function ProtectedRoute({ children }) {
-  const token = localStorage.getItem("token");
-  if (!token) return <Navigate to="/login" replace />;
+export default function ProtectedRoute({ children, role }) {
+  const token = getToken();
+  const user = getUser();
+
+  if (!token || !user) return <Navigate to="/login" />;
+
+  // nếu route yêu cầu role admin mà user không phải admin
+  if (role && user.role !== role) return <Navigate to="/feed" />;
+
   return children;
 }
-//
